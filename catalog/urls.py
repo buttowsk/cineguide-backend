a8ed0e26858e89ec43a1b84e9034f82a9ead2dba
buttowsk <vinicius_nunes0104@hotmail.com>
2023-05-10 17:41:18 -0300

    view para adicionar filmes (temporário)

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from catalog.views import CatalogView, CatalogDetailView, MovieView, CatalogMovieView
+from catalog.views import CatalogView, CatalogDetailView, MovieView, CatalogMovieView, AddMovies
 
 urlpatterns = [
     path('', CatalogView.as_view()),
     path('<int:pk>/', CatalogDetailView.as_view()),
     path('movies/', MovieView.as_view()),
     path('catalog-movies/', CatalogMovieView.as_view()),
+    path('add-movies/', AddMovies.as_view()),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -25,6 +25,7 @@ class CatalogView(APIView):
             status=status.HTTP_400_BAD_REQUEST
         )
 
+
 class CatalogDetailView(APIView):
 
     def verify_pk(self, pk):
@@ -99,3 +100,47 @@ class CatalogMovieView(APIView):
             status=status.HTTP_400_BAD_REQUEST
         )
 
+
+class AddMovies(APIView):
+    def get(self, request):
+        api_key = os.getenv('TMDB_API_KEY')
+        url = f'https://api.themoviedb.org/3/movie/popular?api_key={api_key}&language=en-US&page=1'
+        response = requests.get(url)
+        data = response.json()
+        for movie in data['results']:
+            response = requests.get(
+                f'http://api.themoviedb.org/3/movie/{movie["id"]}?api_key={api_key}&append_to_response=videos,release_dates')
+            movie_data = response.json()
+            br_certification = None
+            us_certification = None
+            for country in movie_data['release_dates']['results']:
+                if country['iso_3166_1'] == 'BR':
+                    br_certification = country['release_dates'][0]['certification']
+                if country['iso_3166_1'] == 'US':
+                    us_certification = country['release_dates'][0]['certification']
+
+            youtube_key = None
+            for video in movie_data['videos']['results']:
+                if video['site'] == 'YouTube' and video['type'] == 'Trailer':
+                    youtube_key = video['key']
+
+            movie = Movie(
+                backdrop=movie_data['backdrop_path'],
+                genres=movie_data['genres'],
+                imdb_id=movie_data['imdb_id'],
+                original_language=movie_data['original_language'],
+                original_title=movie_data['original_title'],
+                overview=movie_data['overview'],
+                poster=movie_data['poster_path'],
+                release_date=movie_data['release_date'],
+                runtime=movie_data['runtime'],
+                tagline=movie_data['tagline'],
+                title=movie_data['title'],
+                vote_average=movie_data['vote_average'],
+                videos=youtube_key,
+                br_certification=br_certification,
+                us_certification=us_certification,
+            )
+            movie.save()
+
+        return Response(status=status.HTTP_200_OK)
