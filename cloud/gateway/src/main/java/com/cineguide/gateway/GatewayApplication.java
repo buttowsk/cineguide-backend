91693d87709c71afc214e2b1bd85f3f07137bb37
Vinícius Nunes <99284437+buttowsk@users.noreply.github.com>
2023-05-18 22:54:25 +0000

    arquitetura

---
+++ cloud/gateway/src/main/java/com/cineguide/gateway/GatewayApplication.java
@@ -0,0 +1,13 @@
+package com.cineguide.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApigatewayApplication.class, args);
+	}
+
+}
